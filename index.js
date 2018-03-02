d7859bee9c1a95e28b39c81e87cf6863f3797a98
char502 <char502@hotmail.com>
2018-03-02 21:04:25 +0000

    targeting properties and custom CSS

--- index.js
+++ index.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
     // target a button with a particular class
     $('#target6').addClass('fadeOut');
     // target a button with a particular ID
+
+    $('button').removeClass('btn-success');
+    // removing a class
+
+    $('#target1').css({ 'color': 'white', backgroundColor: 'blue' });
+    // applying custom css, targeting properties - wrapping the CSS in curly braces allws you to add more than one styling element
+
+    $('#target2').prop('disabled', true);
+    // disable a button by targeting the elements properties
+
+    $('h1').html('Learning <strong>jQuery</strong> is so much fun!');
+    // adding more text
+
+    // $('h1').text('Learning <strong>jQuery</strong> is so much fun!');
+    // can also use .text as shown (but this will include the <strong> markup tags etc)
 });
